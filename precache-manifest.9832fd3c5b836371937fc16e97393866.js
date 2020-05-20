82eff7a314f7e064dbd3ae83ebfc53a7d864865e
Ben Sanders <bensanders@Bens-MacBook-Pro.local>
2020-05-19 23:17:19 -0500

    Updates

---
+++ precache-manifest.9832fd3c5b836371937fc16e97393866.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de996fc863227995a9764ac37ab2a353",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9313c061eaa4f8a5425a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7801bfc80a3313b373ea",
+    "url": "/static/js/2.2407abc4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2407abc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9313c061eaa4f8a5425a",
+    "url": "/static/js/main.5c4a3ad1.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  }
+]);
