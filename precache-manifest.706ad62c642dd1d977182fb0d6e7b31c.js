712b7451c66ef530e0259d6d15876b817058b3ee
Ben Sanders <bensanders@Bens-MacBook-Pro.local>
2020-05-20 16:46:29 -0500

    Updates

---
+++ precache-manifest.706ad62c642dd1d977182fb0d6e7b31c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f11f6d76489e98f1370eb82ad1e1a5f",
+    "url": "/benjaminsanders.me/index.html"
+  },
+  {
+    "revision": "82112b630a336196f789",
+    "url": "/benjaminsanders.me/static/css/main.858814fe.chunk.css"
+  },
+  {
+    "revision": "ad075e68631f83e25fbf",
+    "url": "/benjaminsanders.me/static/js/2.2f5ef2c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/benjaminsanders.me/static/js/2.2f5ef2c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82112b630a336196f789",
+    "url": "/benjaminsanders.me/static/js/main.e1f31358.chunk.js"
+  },
+  {
+    "revision": "3e666e4bae47a7bebc71",
+    "url": "/benjaminsanders.me/static/js/runtime-main.fbd52da7.js"
+  },
+  {
+    "revision": "fc2ff29d679f10daee3680bd3fa9c857",
+    "url": "/benjaminsanders.me/static/media/carPic.fc2ff29d.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f9c8908b0c11d2e8c6aa713281a7e562.js"
+  "/benjaminsanders.me/precache-manifest.706ad62c642dd1d977182fb0d6e7b31c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/benjaminsanders.me/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
